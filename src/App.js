4b9b23bb9c67f3fb92569df15be6489d9c1cdd37
Nick Furfaro <nfurfaro33@gmail.com>
2017-01-12 17:01:12 -0700

    Remove tapEventPlugin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import FontAwesome from 'react-fontawesome';
